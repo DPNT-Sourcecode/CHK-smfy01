4debbc58aeb2c734a1b2b43a1ddb1a1aa563337e
sbx_user1051 <sbx_user1051@169.254.18.181>
2022-06-12 13:44:18 +0000

    Sun Jun 12 13:44:18 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,12 +9,19 @@ class NonexistentItemException(CartException):
     pass
 
 @dataclass
-class SKU:
+class CartItem:
     item: str
     quantity: int
 
     def total_cost(self) -> int:
         return PRICES[self.item] * self.quantity
+
+@dataclass
+class Cart:
+    items: List[CartItem]
+
+    def total_cost(self) -> int:
+        return sum((item.total_cost for item in self.items))
 
 
 PRICES = {
@@ -33,16 +40,21 @@ def checkout(skus):
     except CartException:
         return -1
 
-def parse_cart(skus) -> List[SKU]:
+def parse_cart(skus) -> Cart:
     if not skus:
         return []
 
     cart = Counter(skus)
 
-    if set(cart) -
+    if set(cart) - set(PRICES.keys()):
+        raise NonexistentItemException()
+
+    items = [CartItem(item=item, quantity=quantity) for item, quantity in cart.items()]
+    return Cart(items=items)
 
 
 def price_of_sku(sku: string, quantity: int) -> int:
+
 
 
 
