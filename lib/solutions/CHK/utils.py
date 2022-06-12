c5a64b7d54b5a6a72ed299e0238be879adcac4b5
sbx_user1051 <sbx_user1051@169.254.18.181>
2022-06-12 15:30:00 +0000

    Sun Jun 12 15:30:00 UTC 2022

--- lib/solutions/CHK/utils.py
+++ lib/solutions/CHK/utils.py
@@ -1,11 +1,11 @@
 import re
 
-from lib.solutions.CHK.models import DiscountOffer
+from lib.solutions.CHK.models import DiscountOffer, FreeOffer
 
 LINE_REGEX = r"^| ([A-Z])    | (\d+)\s+| (.*) |$"
 
-FREEBIE_REGEX = r"(\d)([A-Z]) get one [A-Z] free"
-DISCOUNT_REGEX = r"(\d)[A-Z]) for (\d+)"
+FREEBIE_REGEX = r"(\d)[A-Z] get one ([A-Z]) free"
+DISCOUNT_REGEX = r"(\d)[A-Z] for (\d+)"
 
 
 def parse_table(table: str):
@@ -32,43 +32,56 @@ def parse_table(table: str):
             if match:
                 discounts = discount_offers.setdefault(item, {})
                 quantity = match[0]
-                price = match[2]
-                discounts[quantity] = DiscountOffer(item=item, quantity=)
+                cost = match[1]
+                discounts[quantity] = DiscountOffer(
+                    item=item, quantity=quantity, cost=cost
+                )
+                continue
+
+            match = re.match(FREEBIE_REGEX, offer)
+            if match:
+                quantity = match[0]
+                free_item = match[1]
+                freebie_offers[item] = FreeOffer(
+                    item=item, quantity=quantity, free_item=free_item, free_quantity=1
+                )
+
+            raise Exception("Expected an offer")
+
+    return prices, discount_offers, freebie_offers
 
 
+print(
+    parse_table(
+        "| A    | 50    | 3A for 130, 5A for 200 |\n"
+        "| B    | 30    | 2B for 45              |\n"
+        "| C    | 20    |                        |\n"
+        "| D    | 15    |                        |\n"
+        "| E    | 40    | 2E get one B free      |\n"
+        "| F    | 10    | 2F get one F free      |\n"
+        "| G    | 20    |                        |\n"
+        "| H    | 10    | 5H for 45, 10H for 80  |\n"
+        "| I    | 35    |                        |\n"
+        "| J    | 60    |                        |\n"
+        "| K    | 80    | 2K for 150             |\n"
+        "| L    | 90    |                        |\n"
+        "| M    | 15    |                        |\n"
+        "| N    | 40    | 3N get one M free      |\n"
+        "| O    | 10    |                        |\n"
+        "| P    | 50    | 5P for 200             |\n"
+        "| Q    | 30    | 3Q for 80              |\n"
+        "| R    | 50    | 3R get one Q free      |\n"
+        "| S    | 30    |                        |\n"
+        "| T    | 20    |                        |\n"
+        "| U    | 40    | 3U get one U free      |\n"
+        "| V    | 50    | 2V for 90, 3V for 130  |\n"
+        "| W    | 20    |                        |\n"
+        "| X    | 90    |                        |\n"
+        "| Y    | 10    |                        |\n"
+        "| Z    | 50    |                        |"
+    )
+)
 
-
-    return prices
-
-
-print(parse_table(
-    "| A    | 50    | 3A for 130, 5A for 200 |\n"
-    "| B    | 30    | 2B for 45              |\n"
-    "| C    | 20    |                        |\n"
-    "| D    | 15    |                        |\n"
-    "| E    | 40    | 2E get one B free      |\n"
-    "| F    | 10    | 2F get one F free      |\n"
-    "| G    | 20    |                        |\n"
-    "| H    | 10    | 5H for 45, 10H for 80  |\n"
-    "| I    | 35    |                        |\n"
-    "| J    | 60    |                        |\n"
-    "| K    | 80    | 2K for 150             |\n"
-    "| L    | 90    |                        |\n"
-    "| M    | 15    |                        |\n"
-    "| N    | 40    | 3N get one M free      |\n"
-    "| O    | 10    |                        |\n"
-    "| P    | 50    | 5P for 200             |\n"
-    "| Q    | 30    | 3Q for 80              |\n"
-    "| R    | 50    | 3R get one Q free      |\n"
-    "| S    | 30    |                        |\n"
-    "| T    | 20    |                        |\n"
-    "| U    | 40    | 3U get one U free      |\n"
-    "| V    | 50    | 2V for 90, 3V for 130  |\n"
-    "| W    | 20    |                        |\n"
-    "| X    | 90    |                        |\n"
-    "| Y    | 10    |                        |\n"
-    "| Z    | 50    |                        |"
-))
 
 
 
