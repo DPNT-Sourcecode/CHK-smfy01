00fea8c1e08754e0f167bf1e01709822ff0bf145
sbx_user1051 <sbx_user1051@169.254.18.181>
2022-06-12 14:06:14 +0000

    Sun Jun 12 14:06:14 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,11 +2,14 @@ from collections import Counter
 from dataclasses import dataclass
 from typing import List
 
+
 class CartException(Exception):
     pass
 
+
 class NonexistentItemException(CartException):
     pass
+
 
 @dataclass
 class CartItem:
@@ -16,14 +19,17 @@ class CartItem:
     def is_on_offer(self) -> bool:
         return self.item in OFFERS
 
-    def total_cost(self) -> int:
-        if not self.is_on_offer():
-            return PRICES[self.item] * self.quantity
-
+    def offer_cost(self) -> int:
         offer = OFFERS[self.item]
         quantity_of_offer, remainder = divmod(self.quantity, offer.quantity)
 
-        return PRICES[self.item] * remainder + offer.cost * quantity_of_offer
+        return UNIT_COSTS[self.item] * remainder + offer.cost * quantity_of_offer
+
+    def total_cost(self) -> int:
+        if not self.is_on_offer():
+            return UNIT_COSTS[self.item] * self.quantity
+
+        return self.offer_cost()
 
 
 @dataclass
@@ -33,6 +39,7 @@ class Cart:
     def total_cost(self) -> int:
         return sum((item.total_cost() for item in self.items))
 
+
 @dataclass
 class Offer:
     item: str
@@ -40,16 +47,14 @@ class Offer:
     cost: int
 
 
-PRICES = {
-    "A": 50,
-    "B": 30,
-    "C": 20,
-    "D": 15
-}
+UNIT_COSTS = {"A": 50, "B": 30, "C": 20, "D": 15}
 
 OFFERS = {
-    "A": Offer(item="A", quantity=3, cost=130),
-    "B": Offer(item="B", quantity=2, cost=45)
+    "A": {
+        3: Offer(item="A", quantity=3, cost=130),
+        5: Offer(item="A", quantity=5, cost=200),
+    },
+    "B": Offer(item="B", quantity=2, cost=45),
 }
 
 
@@ -63,15 +68,17 @@ def checkout(skus):
 
     return cart.total_cost()
 
+
 def parse_cart(skus) -> Cart:
     if not skus:
         return Cart(items=[])
 
     cart = Counter(skus)
 
-    if set(cart) - set(PRICES.keys()):
+    if set(cart) - set(UNIT_COSTS.keys()):
         raise NonexistentItemException()
 
     items = [CartItem(item=item, quantity=quantity) for item, quantity in cart.items()]
     return Cart(items=items)
+
 
